3b2667191c601c41f6f5a50ca4a29de9bab9f8d5
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-07-07 15:43:40 +0200

    exercice 2

---
+++ exercices_boucles/exo2/script.js
@@ -0,0 +1,9 @@
+//let letters = [ 'A', 'd', 'a', '-', 'T', 'e', 'c', 'h']
+let letters = [ 'A', 'd', 'a', '-', 'S', 'c', 'h', 'o', 'o', 'l']
+let message=""
+
+for(let letter of letters){
+    message +=letter
+    
+}
+console.log(message)
