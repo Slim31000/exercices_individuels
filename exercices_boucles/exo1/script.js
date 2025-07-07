3b89f1f1fb04e015706ae1161fec81a0558df574
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-07-07 16:16:25 +0200

    exercice boucle exo1

---
+++ exercices_boucles/exo1/script.js
@@ -0,0 +1,20 @@
+for(let i=0;i<100;i++){
+    console.log("boucle for:",i)
+}
+console.log("----------------------------")
+
+let i=0
+while(i<100){
+    console.log("boucle while:",i)
+    i++
+}
+console.log("----------------------------")
+
+for(let j=50;j<100;j++){
+    console.log("j:",j)
+}
+console.log("----------------------------")
+
+for(let j=49;j>=1;j--){
+    console.log("j:",j)
+}
