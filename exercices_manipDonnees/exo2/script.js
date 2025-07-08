e8e800a8645209e03e055c5a054d912e6ab4f5f4
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-07-08 10:25:40 +0200

    exercice 2 of manip data

---
+++ exercices_manipDonnees/exo2/script.js
@@ -0,0 +1,34 @@
+const obj = {
+  "candidate": "Le Filip",
+  "age": 29,
+  "city": "Paris",
+  "region": "Île-de-France"
+}
+
+const data = [
+	{
+		"candidate": "Afrodite Amour",
+		"age": 27,
+		"city": "Lyon",
+		"region": "Auvergne-Rhône-Alpes"
+	},
+	{
+		"candidate": "Edeha Noire",
+		"age": 34,
+		"city": "Lyon",
+		"region": "Auvergne-Rhône-Alpes"
+	},
+	{
+		"candidate": "Le Filip",
+		"age": 29,
+		"city": "Paris",
+		"region": "Île-de-France"
+	}
+]
+
+console.log(obj.candidate)
+console.log("----------------------------")
+
+data.forEach( obj=> {
+    console.log(obj.candidate)
+});
