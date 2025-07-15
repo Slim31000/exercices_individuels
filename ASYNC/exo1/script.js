2d1f86d956ab2d873a849100990ce7c310dff0d9
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-07-15 18:00:30 +0200

    ajout d'un loader de chargement

--- ASYNC/exo1/script.js
+++ ASYNC/exo1/script.js
@@ -2,23 +2,27 @@
 
 async function fetchOffres() {
     try {
+        const loader = document.getElementById('loader');
+        loader.style.display='flex'
         const response = await fetch('https://api.allorigins.win/raw?url=https://codepassport.dev/api/offers');
-    const result= await response.json()
-    console.log(result)
-    console.log(result[0].description)
-   for(let i=0; i<result.length;i++){
-    const titre= document.createElement('h2')
-    titre.textContent = result[i].titre
-    const descriptif=document.createElement('p')
-    descriptif.textContent = result[i].description
-    document.body.appendChild(titre)
-    document.body.appendChild(descriptif)
-   }
+        const result = await response.json()
+        console.log(result)
+        console.log(result[0].description)
+        for (let i = 0; i < result.length; i++) {
+            const titre = document.createElement('h2')
+            titre.textContent = result[i].titre
+            const descriptif = document.createElement('p')
+            descriptif.textContent = result[i].description
+            loader.style.display='none'
+            document.body.appendChild(titre)
+            document.body.appendChild(descriptif)
+
+        }
     } catch (error) {
-        console.log('api non trouve',error)
+        console.log('api non trouve', error)
     }
-    
-    
+
+
 }
 
 fetchOffres()
