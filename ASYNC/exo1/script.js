8d9652fee08c09ec3fa2c5d94f3f1342bfb19e53
Slim31000 <123716668+Slim31000@users.noreply.github.com>
2025-07-15 10:45:55 +0200

    add try catch

--- ASYNC/exo1/script.js
+++ ASYNC/exo1/script.js
@@ -1,18 +1,22 @@
 
 
 async function fetchOffres() {
-    const response = await fetch('https://api.allorigins.win/raw?url=https://codepassport.dev/api/offers');
+    try {
+        const response = await fetch('https://api.allorigins.win/raw?url=https://codepassport.dev/api/offers');
     const result= await response.json()
     console.log(result)
     console.log(result[0].description)
    for(let i=0; i<result.length;i++){
     const titre= document.createElement('h2')
-    titre.innerHTML = `<h2>${result[i].titre}</h2>`
+    titre.textContent = result[i].titre
     const descriptif=document.createElement('p')
-    descriptif.innerHTML = `<p>${result[i].description}</p>`
+    descriptif.textContent = result[i].description
     document.body.appendChild(titre)
     document.body.appendChild(descriptif)
    }
+    } catch (error) {
+        console.log('api non trouve',error)
+    }
     
     
 }
